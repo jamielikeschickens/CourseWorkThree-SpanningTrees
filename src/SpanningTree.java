5f6986647fbb65e53648678f2670c6015096f356
Will Price <will.price94@gmail.com>
2014-02-16 20:28:49 +0000

    Completed Part 1
    * Main method handles '-p1' parameter
    * Need to refactor tests for part one to reduce duplication of test
    pattern
    * Add graph.txt to repository
    * Started working on calculating the cost of edges

--- src/SpanningTree.java
+++ src/SpanningTree.java
@@ -1,14 +1,57 @@
+import java.io.IOException;
+
 
 public class SpanningTree {
-
 	private Graph graph;
 
+	
 	public SpanningTree(Graph graph) {
 		this.graph = graph;
 	}
-
-	public int calculateTotalEdgeWeight() {
-		return graph.edges().size();
+		
+	public SpanningTree() {
+	}
+	
+	public static void main(String[] args) {
+		Graph graph = readGraphFromFile(args[1]);
+		SpanningTree spanningTree = new SpanningTree(graph);
+		if (isPartOne(args)) {
+			double totalCable = spanningTree.calculateTotalEdgeWeight();
+			String output = "Total Cable Needed: " +
+							Double.toString(totalCable) +
+							"m\n";
+			
+			println(output);
+		}
 	}
 
+	public double calculateTotalEdgeWeight() {
+		double totalWeight = 0;
+		for (Edge edge : graph.edges()) {
+			totalWeight += edge.weight();
+		};
+		return totalWeight;
+	}
+
+	public double calculateCostOfEdge(Edge edge) {
+		return 0.2 * edge.weight();
+	}
+	
+	private static Graph readGraphFromFile(String filename) {
+		Reader reader = new Reader();
+		try {
+			reader.read(filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return reader.graph();
+	}
+
+	private static void println(String output) {
+		System.out.println(output);
+	}
+	
+	private static boolean isPartOne(String[] args) {
+		return args[0].equals("-p1");
+	}
 }
--- tests/SpanningTreeTests.java
+++ tests/SpanningTreeTests.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -7,7 +7,8 @@ import org.junit.Test;
 public class SpanningTreeTests {
 
 	private Graph graph;
-
+	private final double maxError = 0.001;
+	
 	@Before
 	public void setUp() {
 		graph = new Graph();
@@ -15,27 +16,70 @@ public class SpanningTreeTests {
 	
 	@Test
 	public void emptyGraphHasWeightZero() {
-		assertEquals(0, calculateTotalEdgeWeightInGraph());
+		assertEquals(0, calculateTotalEdgeWeightInGraph(), maxError);
 	}
 
 	@Test
 	public void graphWithOneEdgeOfWeightOneHasTotalWeightOne() throws Exception {
-		Edge edge = new Edge("", "", 1, Edge.EdgeType.LocalRoad);
+		Edge edge = createNewEdge(1);
 		graph.add(edge);
 		
-		assertEquals(1, calculateTotalEdgeWeightInGraph());
+		assertEquals(1, calculateTotalEdgeWeightInGraph(), maxError);
 	}
 	
 	@Test
 	public void graphWithOneEdgeOfWeightTwoHasTotalWeightTwo() throws Exception {
-		Edge edge = new Edge("", "", 2, Edge.EdgeType.LocalRoad);
+		Edge edge = createNewEdge(2);
 		graph.add(edge);
 		
-		assertEquals(2, calculateTotalEdgeWeightInGraph());
+		assertEquals(2, calculateTotalEdgeWeightInGraph(), maxError);
+	}
+	
+	@Test
+	public void graphWithTwoEdgesOfWeightOneHasTotalWeightTwo() throws Exception {
+		Edge edge = createNewEdge(1);
+		Edge edgeTwo = createNewEdge(1);
+		graph.add(edge);
+		graph.add(edge);
+		
+		assertEquals(2, calculateTotalEdgeWeightInGraph(), maxError);
+	}
+	
+	@Test
+	public void costOfOneKilometerOfLocalRoadInHours() throws Exception {
+		Edge edge = createNewEdge(1, Edge.EdgeType.LocalRoad);
+		double costOfEdge = calculateCostOfEdge(edge);
+		
+		assertEquals(0.2, costOfEdge, maxError);
+	}
+	
+	@Test
+	public void costOfTwoKilometersOfLocalRoadInHours() throws Exception {
+		Edge edge = createNewEdge(2, Edge.EdgeType.LocalRoad);
+		double costOfEdge = calculateCostOfEdge(edge);
+		
+		assertEquals(0.4, costOfEdge, maxError);
+	}
+
+	private double calculateCostOfEdge(Edge edge) {
+		// Note to self:
+		// This is useful because maybe calculateCostOfEdge should be private
+		// in which case we'd need to create a new graph here and add the edge,
+		// then pass that into a SpanningTree and calculate the cost of all the edges
+		return new SpanningTree().calculateCostOfEdge(edge);
+	}
+	
+	
+	private Edge createNewEdge(double length) {
+		return new Edge("", "", length, Edge.EdgeType.LocalRoad);
+	}
+	
+	private Edge createNewEdge(double length, Edge.EdgeType type) {
+		return new Edge("", "", length, type);
 	}
 	
 
-	private int calculateTotalEdgeWeightInGraph() {
+	private double calculateTotalEdgeWeightInGraph() {
 		return new SpanningTree(graph).calculateTotalEdgeWeight();
 	}
 }
